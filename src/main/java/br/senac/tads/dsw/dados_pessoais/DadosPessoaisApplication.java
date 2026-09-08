7e585cdffea3548e5454f455a73fe8e8cff957eb
Eryka Marcal <erykamarcalt@gmail.com>
2026-09-08 01:35:01 -0300

    Etapa 0: projeto gerado pelo Spring Initializr

---
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 10f31d02f7632a527ced87a8a16a60109e7a7f6e9d2bb530dc8fff7f2f538918
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 10f31d02f7632a527ced87a8a16a60109e7a7f6e9d2bb530dc8fff7f2f538918
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
